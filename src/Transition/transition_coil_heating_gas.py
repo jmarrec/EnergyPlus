e06751f89f2c3bf894570cbf3ced422d560f2a92
Julien Marrec <julien.marrec@gmail.com>
2025-10-10 11:57:37 +0200

    transition_coil_heating was using python 2 print syntax and black choked on it

--- src/Transition/transition_coil_heating_gas.py
+++ src/Transition/transition_coil_heating_gas.py
@@ -59,42 +59,43 @@ import re
 
 def add_fuel_type(filename):
     assert os.path.isfile(filename)
-    with open(filename, 'rU') as f:
+    with open(filename, "rU") as f:
         filecontents = f.read()
 
     def repl(m):
-        if m.group(2) == 'Coil:Heating:Gas':
+        if m.group(2) == "Coil:Heating:Gas":
             try:
-                itemlist = m.group(3).split(',')
-                pre_whitespace = re.match(r'\s*', itemlist[1].split('\n')[1]).group()
-                width = len(itemlist[1].split('\n')[1]) + 1 + len(re.match(r'\s*', itemlist[2]).group())
-                width -= len(pre_whitespace) + len('NaturalGas,')
-                post_whitespace = ' ' * width
-                pre, post = itemlist[2].split('\n')
-                itemlist[2] = '{}\n{}NaturalGas,{}!- FuelType\n{}'.format(pre, pre_whitespace, post_whitespace, post)
+                itemlist = m.group(3).split(",")
+                pre_whitespace = re.match(r"\s*", itemlist[1].split("\n")[1]).group()
+                width = len(itemlist[1].split("\n")[1]) + 1 + len(re.match(r"\s*", itemlist[2]).group())
+                width -= len(pre_whitespace) + len("NaturalGas,")
+                post_whitespace = " " * width
+                pre, post = itemlist[2].split("\n")
+                itemlist[2] = "{}\n{}NaturalGas,{}!- FuelType\n{}".format(pre, pre_whitespace, post_whitespace, post)
 
-                group3 = ','.join(itemlist)
-                return m.group(1) + m.group(2) + ',' + group3 + ';'
+                group3 = ",".join(itemlist)
+                return m.group(1) + m.group(2) + "," + group3 + ";"
             except:
                 return m.group()
         else:
             return m.group()
 
-    newfilecontents = re.sub(r'(\s*)([\w:]+),(.*?);', repl, filecontents, flags=re.DOTALL)
+    newfilecontents = re.sub(r"(\s*)([\w:]+),(.*?);", repl, filecontents, flags=re.DOTALL)
 
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         f.write(newfilecontents)
+
 
 def main():
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    example_file_dir = os.path.abspath(os.path.join(this_dir, '..', '..', 'testfiles'))
+    example_file_dir = os.path.abspath(os.path.join(this_dir, "..", "..", "testfiles"))
     for dirpath, dirnames, filenames in os.walk(example_file_dir):
-        print dirpath
+        print(dirpath)
         for filename in filenames:
-            if filename.endswith('.idf'):
-                print '  {}'.format(filename)
+            if filename.endswith(".idf"):
+                print("  {filename}".format())
                 add_fuel_type(os.path.join(dirpath, filename))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
