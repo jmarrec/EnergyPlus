44e882becb3e84cbdd3a04f7ae6a38df3e17981c
Yujie Xu <yujiex@Yujies-MBP.dhcp.lbnl.us>
2025-10-31 11:12:22 -0700

    update error message, IO ref for water loop type

--- src/EnergyPlus/PlantLoopHeatPumpEIR.cc
+++ src/EnergyPlus/PlantLoopHeatPumpEIR.cc
@@ -2590,7 +2590,7 @@ void EIRPlantLoopHeatPump::oneTimeInit(EnergyPlusData &state)
                                        state.dataPlnt->PlantLoop(this->loadSidePlantLoc.loopNum).Name,
                                        "HeatPump:AirToWater",
                                        this->name));
-                ShowContinueError(state, "The load and source sides need to be on different loops.");
+                ShowContinueError(state, "The Hot Water nodes must be connected to a HotWater loop. The Chilled Water nodes must be ChilledWater loop");
                 errFlag = true;
             }
         }
