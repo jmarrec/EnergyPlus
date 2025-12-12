a9dc0370d74810af2bde168c718b0663f8f9fd6d
Dareum Nam <iamdareum@gmail.com>
2025-12-12 00:01:12 -0700

    fix errors 5

--- src/EnergyPlus/HeatBalanceHAMTManager.cc
+++ src/EnergyPlus/HeatBalanceHAMTManager.cc
@@ -183,7 +183,9 @@ namespace HeatBalanceHAMTManager {
         int NumAlphas;
         int status;
         int Numid;
+
         int HAMTitems;
+        int vtcsid;
 
         bool ErrorsFound;
 
@@ -596,7 +598,6 @@ namespace HeatBalanceHAMTManager {
 
         // Vapor Transfer coefficients
         HAMTitems = s_ip->getNumObjectsFound(state, cHAMTObject7); // SurfaceProperties:VaporCoefficients
-        int vtcsid = Util::FindItemInList(AlphaArray(1), state.dataSurface->Surface);
         for (int item = 1; item <= HAMTitems; ++item) {
             s_ip->getObjectItem(state,
                                 cHAMTObject7,
@@ -612,6 +613,7 @@ namespace HeatBalanceHAMTManager {
                                 cNumericFieldNames);
 
             ErrorObjectHeader eoh{routineName, cHAMTObject7, AlphaArray(1)};
+            vtcsid = Util::FindItemInList(AlphaArray(1), state.dataSurface->Surface);
             if (vtcsid == 0) {
                 ShowSevereItemNotFound(state, eoh, cAlphaFieldNames(1), AlphaArray(1));
                 ShowContinueError(state, "The basic material must be defined in addition to specifying HeatAndMoistureTransfer properties.");
