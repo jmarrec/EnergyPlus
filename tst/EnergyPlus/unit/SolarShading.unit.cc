6a77e9153827e6d558f57e5833758447d2520244
Joe Robertson <joseph.robertson@nrel.gov>
2026-03-03 10:35:32 -0700

    Improve and add unit tests for solar shading.

--- tst/EnergyPlus/unit/SolarShading.unit.cc
+++ tst/EnergyPlus/unit/SolarShading.unit.cc
@@ -5262,6 +5262,47 @@ TEST_F(EnergyPlusFixture, SolarShadingTest_checkScheduledSurfacePresent)
 // Test of check to see if all surfaces have a sunlit schedule when shadow calculations are set to "Scheduled"
 {
     auto &surfData = state->dataSurface;
+    surfData->TotSurfaces = 6;
+    surfData->Surface.allocate(surfData->TotSurfaces);
+
+    // Set up data for test: three of five surfaces are non-shading and two are shading.  Shading surfaces
+    // are skipped so they should not report any errors.  Two of the surfaces will be correctly defined while
+    // one surface will not be correctly defined and will generate an error.
+    surfData->Surface(1).Class = SurfaceClass::Wall;
+    surfData->Surface(1).SurfSchedExternalShadingFrac = true;
+    surfData->Surface(1).Name = "WALL1OK";
+    surfData->Surface(2).Class = SurfaceClass::Roof;
+    surfData->Surface(2).SurfSchedExternalShadingFrac = true;
+    surfData->Surface(2).Name = "ROOF1OK";
+    surfData->Surface(3).Class = SurfaceClass::Window;
+    surfData->Surface(3).SurfSchedExternalShadingFrac = false;
+    surfData->Surface(3).Name = "WINDOW1NOTOK";
+    surfData->Surface(4).Class = SurfaceClass::Shading;
+    surfData->Surface(4).SurfSchedExternalShadingFrac = true;
+    surfData->Surface(4).Name = "SHADING1OK";
+    surfData->Surface(5).Class = SurfaceClass::Overhang;
+    surfData->Surface(5).SurfSchedExternalShadingFrac = false;
+    surfData->Surface(5).Name = "SHADING2NOTOK";
+    surfData->Surface(6).Class = SurfaceClass::Window;
+    surfData->Surface(6).SurfSchedExternalShadingFrac = false;
+    surfData->Surface(6).Name = "WINDOW2NOTOK";
+
+    checkScheduledSurfacePresent(*state);
+
+    std::string const error_string = delimited_string({
+        "   ** Warning ** ShadowCalculation specified Schedule for the Shading Calculation Method but no schedule provided for WINDOW1NOTOK.",
+        "   **   ~~~   ** When Schedule is selected for the Shading Calculation Method and no schedule is provided for a particular surface,",
+        "   **   ~~~   ** EnergyPlus will assume that the surface is not shaded.  Use SurfaceProperty:LocalEnvironment to specify a schedule",
+        "   **   ~~~   ** for sunlit fraction if this was not desired.  Otherwise, this surface will not be shaded at all.",
+        "   ** Warning ** No schedule was provided for WINDOW2NOTOK either.  See above error message for more details.",
+    });
+    EXPECT_TRUE(compare_err_stream(error_string, true));
+}
+
+TEST_F(EnergyPlusFixture, SolarShadingTest_checkNotScheduledSurfacePresent)
+// Test of check to see if all surfaces do not have a sunlit schedule when shadow calculations are not set to "Scheduled"
+{
+    auto &surfData = state->dataSurface;
     surfData->TotSurfaces = 5;
     surfData->Surface.allocate(surfData->TotSurfaces);
 
@@ -5284,16 +5325,15 @@ TEST_F(EnergyPlusFixture, SolarShadingTest_checkScheduledSurfacePresent)
     surfData->Surface(5).SurfSchedExternalShadingFrac = false;
     surfData->Surface(5).Name = "SHADING2NOTOK";
 
-    checkScheduledSurfacePresent(*state);
+    checkNotScheduledSurfacePresent(*state);
 
     std::string const error_string = delimited_string({
-        "   ** Warning ** ShadowCalculation specified Schedule for the Shading Calculation Method but no schedule provided for WINDOW1NOTOK",
-        "   **   ~~~   ** When Schedule is selected for the Shading Calculation Method and no schedule is provided for a particular surface,",
-        "   **   ~~~   ** EnergyPlus will assume that the surface is not shaded.  Use SurfaceProperty:LocalEnvironment to specify a schedule",
-        "   **   ~~~   ** for sunlit fraction if this was not desired.  Otherwise, this surface will not be shaded at all.",
+        "   ** Warning ** ShadowCalculation did not specify Schedule for the Shading Calculation Method but schedule provided for WALL1OK.",
+        "   ** Warning ** Schedule was also provided for ROOF1OK.  See above error message for more details.",
     });
     EXPECT_TRUE(compare_err_stream(error_string, true));
 }
+
 TEST_F(EnergyPlusFixture, SolarShadingTest_CalcBeamSolarOnWinRevealSurface)
 {
     state->dataGlobal->TimeStepsInHour = 6;
