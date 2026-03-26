f4698b3dbf2acec1a0ef4ee2484f9f0226bb734d
rraustad <rraustad@fsec.ucf.edu>
2026-03-25 23:43:25 -0400

    Fix unit test

--- src/EnergyPlus/SimAirServingZones.cc
+++ src/EnergyPlus/SimAirServingZones.cc
@@ -6757,14 +6757,16 @@ void UpdateSysSizing(EnergyPlusData &state, Constant::CallIndicator const CallIn
                 // HeatMixHumRat isn't calculated correctly when there is no heating load but heating mass flow rate > 0
                 // use min HeatMixHumRat for sizing certain objects, e.g., humidifiers
                 if (state.dataSize->CalcSysSizing(AirLoopNum).HeatOAOption == DataSizing::OAControl::MinOA) {
-                    OutAirFrac = RhoAir * state.dataSize->CalcSysSizing(AirLoopNum).DesOutAirVolFlow /
-                                 state.dataSize->CalcSysSizing(AirLoopNum).NonCoinHeatMassFlow;
+                    OutAirFrac = (state.dataSize->CalcSysSizing(AirLoopNum).NonCoinHeatMassFlow > 0.0)
+                                     ? RhoAir * state.dataSize->CalcSysSizing(AirLoopNum).DesOutAirVolFlow /
+                                           state.dataSize->CalcSysSizing(AirLoopNum).NonCoinHeatMassFlow
+                                     : 1.0;
                     OutAirFrac = min(1.0, max(0.0, OutAirFrac));
                 } else {
                     OutAirFrac = 1.0;
                 }
                 for (int curSimDay = 1; curSimDay <= state.dataSize->SysSizing.u1(); ++curSimDay) {
-                    for (int ts = 1; ts <= 24.0 * state.dataGlobal->TimeStepsInHour; ++ts) {
+                    for (int ts = 1; ts <= Constant::iHoursInDay * state.dataGlobal->TimeStepsInHour; ++ts) {
                         SysHeatRetHumRat = state.dataSize->SysSizing(curSimDay, AirLoopNum).SysHeatRetHumRatSeq(ts);
                         OutAirHumRat = state.dataSize->SysSizing(curSimDay, AirLoopNum).SysHeatOutHumRatSeq(ts);
                         SysHeatMixHumRat = OutAirHumRat * OutAirFrac + SysHeatRetHumRat * (1.0 - OutAirFrac);
