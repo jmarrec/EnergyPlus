0a5af7b18883b804b3ab563613756d931e4a830a
Dareum Nam <iamdareum@gmail.com>
2026-04-24 00:29:01 -0600

    fix constVariablePointer error

--- src/EnergyPlus/AirflowNetwork/src/Solver.cpp
+++ src/EnergyPlus/AirflowNetwork/src/Solver.cpp
@@ -5066,12 +5066,12 @@ namespace AirflowNetwork {
         if (simulation_control.DuctLoss) {
             // Assign node num based on Distribution node
             for (int i = 1; i <= AirflowNetworkNumOfLinks; ++i) {
-                for (int j = 1; j <= DisSysNumOfNodes; ++j) {
-                    if (Util::SameString(AirflowNetworkLinkageData(i).NodeNames[0], DisSysNodeData(j).Name)) {
-                        AirflowNetworkLinkageData(i).NodeNums[0] = j;
+                for (int k = 1; k <= DisSysNumOfNodes; ++k) {
+                    if (Util::SameString(AirflowNetworkLinkageData(i).NodeNames[0], DisSysNodeData(k).Name)) {
+                        AirflowNetworkLinkageData(i).NodeNums[0] = k;
                     }
-                    if (Util::SameString(AirflowNetworkLinkageData(i).NodeNames[1], DisSysNodeData(j).Name)) {
-                        AirflowNetworkLinkageData(i).NodeNums[1] = j;
+                    if (Util::SameString(AirflowNetworkLinkageData(i).NodeNames[1], DisSysNodeData(k).Name)) {
+                        AirflowNetworkLinkageData(i).NodeNums[1] = k;
                     }
                 }
                 if (AirflowNetworkLinkageData(i).NodeNums[0] == 0) {
