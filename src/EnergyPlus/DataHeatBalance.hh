9d4ec348f409b79f61243d5bd648ccdb563a63ff
Joe Robertson <joseph.robertson@nrel.gov>
2026-05-21 16:10:43 -0700

    Update and simplify new baseboard sizing code based on feedback.

--- src/EnergyPlus/DataHeatBalance.hh
+++ src/EnergyPlus/DataHeatBalance.hh
@@ -1076,13 +1076,7 @@ namespace DataHeatBalance {
         Real64 EMSZoneBaseboardPower = 0.0;      // Value EMS is directing to use for override
         Real64 FractionRadiant = 0.0;
         Real64 ZnHtgSetTemp = 0.0;          // Design zone heating setpoint temperature
-        Real64 ZnMinOutTemp = 0.0;          // Minimum outdoor temperature over design days
         Real64 ExtSurfCondLoad = 0.0;       // Conductional load through exterior surfaces by max temp diff
-        bool IsThisSized = false;           // True if called once
-        bool InfilVentDone = false;         // True if infiltration / ventilation loads were determined
-                                            // It is also used to skip second sizing call if necessary
-        bool CapatLowTempAutosize = false;  // Flag to indicate autosize of the field in second sizing call
-        bool CapatHighTempAutosize = false; // Flag to indicate autosize of the field in second sizing call
         Real64 FractionConvected = 0.0;
         bool ManageDemand = false; // Flag to indicate whether to use demand limiting
         Real64 DemandLimit = 0.0;  // Demand limit set by demand manager [W]
@@ -1096,6 +1090,7 @@ namespace DataHeatBalance {
         Real64 ConGainEnergy = 0.0;    // Convective heat gain [J]
         Real64 TotGainEnergy = 0.0;    // Total heat gain [J]
         std::string EndUseSubcategory; // user defined name for the end use category
+        bool MySizeFlag = true;
     };
 
     struct InfiltrationData
--- src/EnergyPlus/InternalHeatGains.hh
+++ src/EnergyPlus/InternalHeatGains.hh
@@ -121,7 +121,7 @@ namespace InternalHeatGains {
 
     void InitInternalHeatGains(EnergyPlusData &state);
 
-    void SizeOaControlledBaseboard(EnergyPlusData &state);
+    void SizeOaControlledBaseboard(EnergyPlusData &state, int BaseboardNum);
 
     void CheckReturnAirHeatGain(EnergyPlusData &state);
 
