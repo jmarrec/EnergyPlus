0e2c98501276473759df4d8f57e24ee79312b157
Julien Marrec <julien.marrec@gmail.com>
2026-06-02 16:29:53 +0200

    Bump energyplus_transition_tools to 2.1.5
    https://github.com/Myoldmopar/EnergyPlusTransitionTools/releases/tag/v2.1.5

--- cmake/PythonInstallCLITools.py
+++ cmake/PythonInstallCLITools.py
@@ -61,7 +61,7 @@ from sys import executable
 
 PKGS = {
     "energyplus_launch": "3.7.4",
-    "energyplus_transition_tools": "2.1.4",
+    "energyplus_transition_tools": "2.1.5",
     "ghedesigner": "2.1.1",
 }
 
