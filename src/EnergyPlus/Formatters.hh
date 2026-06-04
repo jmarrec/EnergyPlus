a8c24df4cbfaa4bae5d00f439a61f448aab1d3cf
Julien Marrec <julien.marrec@gmail.com>
2026-06-04 17:11:23 +0200

    Implement C++20 versions for future std::format functions: formatting a range (C++26), std::print (C++23)
    For the string one, I went with the same behavior as fmt
    
    ```
        std::vector<std::string> v{"element1", "element2", "element3"};
        fmt::print("{}\n", v);
    
    => ["element1", "element2", "element3"]
    ```

---
+++ src/EnergyPlus/Formatters.hh
@@ -0,0 +1,130 @@
+// EnergyPlus, Copyright (c) 1996-present, The Board of Trustees of the University of Illinois,
+// The Regents of the University of California, through Lawrence Berkeley National Laboratory
+// (subject to receipt of any required approvals from the U.S. Dept. of Energy), Oak Ridge
+// National Laboratory, managed by UT-Battelle, Alliance for Energy Innovation, LLC, and other
+// contributors. All rights reserved.
+//
+// NOTICE: This Software was developed under funding from the U.S. Department of Energy and the
+// U.S. Government consequently retains certain rights. As such, the U.S. Government has been
+// granted for itself and others acting on its behalf a paid-up, nonexclusive, irrevocable,
+// worldwide license in the Software to reproduce, distribute copies to the public, prepare
+// derivative works, and perform publicly and display publicly, and to permit others to do so.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+// (1) Redistributions of source code must retain the above copyright notice, this list of
+//     conditions and the following disclaimer.
+//
+// (2) Redistributions in binary form must reproduce the above copyright notice, this list of
+//     conditions and the following disclaimer in the documentation and/or other materials
+//     provided with the distribution.
+//
+// (3) Neither the name of the University of California, Lawrence Berkeley National Laboratory,
+//     the University of Illinois, U.S. Dept. of Energy nor the names of its contributors may be
+//     used to endorse or promote products derived from this software without specific prior
+//     written permission.
+//
+// (4) Use of EnergyPlus(TM) Name. If Licensee (i) distributes the software in stand-alone form
+//     without changes from the version obtained under this License, or (ii) Licensee makes a
+//     reference solely to the software portion of its product, Licensee must refer to the
+//     software as "EnergyPlus version X" software, where "X" is the version number Licensee
+//     obtained under this License and may not use a different name for the software. Except as
+//     specifically required in this Section (4), Licensee shall not use in a company name, a
+//     product name, in advertising, publicity, or other promotional activities any name, trade
+//     name, trademark, logo, or other designation of "EnergyPlus", "E+", "e+" or confusingly
+//     similar designation, without the U.S. Department of Energy's prior written consent.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef Formatters_hh_INCLUDED
+#define Formatters_hh_INCLUDED
+
+#include <concepts>
+#include <cstdio>
+#include <format>
+#include <ranges>
+#include <string>
+
+#if __cplusplus >= 202302L
+#    error "std::formattable is defined, remove C++ 20 implementation"
+#endif
+
+template <typename T, typename CharT = char>
+concept formattable =
+    requires(std::remove_reference_t<T> & v, std::basic_format_context<std::back_insert_iterator<std::basic_string<CharT>>, CharT> ctx)
+{
+    std::formatter<std::remove_reference_t<T>, CharT>{}.format(v, ctx);
+    std::formatter<std::remove_reference_t<T>, CharT>{}.parse(std::declval<std::basic_format_parse_context<CharT> &>());
+};
+
+template <typename T>
+concept formattable_range =
+    // A range
+    std::ranges::range<T> // A range
+    // A range of formattable elements. It'd be a nicer error message to include it, but that prevents using it for a range of range
+    // eg : std::vector<std::vector<int>>.
+    // && std::formattable<std::ranges::range_value_t<T>, char>
+    // But we exclude the ones that are already formattable as a whole, like std::string and std::string_view, char*.
+    && !std::is_same_v<T, std::string> && !std::is_same_v<T, std::string_view> && !std::is_same_v<std::ranges::range_value_t<T>, char>;
+
+template <formattable_range Container> struct std::formatter<Container>
+{
+    using T = std::ranges::range_value_t<Container>;
+    std::formatter<T> element_formatter;
+
+    constexpr auto parse(std::format_parse_context &ctx) -> std::format_parse_context::iterator
+    {
+        return element_formatter.parse(ctx);
+    }
+
+    auto format(const Container &v, std::format_context &ctx) const -> std::format_context::iterator
+    {
+        auto it = ctx.out();
+        *it++ = '[';
+        bool first = true;
+        for (const auto &elem : v) {
+            if (!first) {
+                it = std::format_to(it, ", ");
+            }
+            first = false;
+            if constexpr (std::is_same_v<T, std::string> || std::is_same_v<T, std::string_view>) {
+                it = std::format_to(it, "\"");
+                it = element_formatter.format(elem, ctx);
+                it = std::format_to(it, "\"");
+            } else {
+                it = element_formatter.format(elem, ctx);
+            }
+        }
+        *it++ = ']';
+        return it;
+    }
+};
+
+#ifdef __cpp_lib_print
+#    error "std::print is defined, remove C++ 20 implementation"
+#endif
+
+namespace std {
+template <class... Args> void print(FILE *stream, std::format_string<Args...> fmt, Args &&...args)
+{
+    std::string formatted = std::format(fmt, std::forward<Args>(args)...);
+    if (std::fwrite(formatted.data(), 1, formatted.size(), stream) != formatted.size()) {
+        throw std::system_error(EIO, std::generic_category());
+    }
+}
+template <class... Args> void print(std::format_string<Args...> fmt, Args &&...args)
+{
+    print(stdout, fmt, std::forward<Args>(args)...);
+}
+} // namespace std
+
+#endif
