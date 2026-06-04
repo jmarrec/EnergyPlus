dad65880c9f0cfa24615ba9354f3f7851ba8c961
Joe Robertson <joseph.robertson@nrel.gov>
2026-06-04 08:29:41 -0700

    Formatting and clean up use of finalSysSizing.

--- src/EnergyPlus/VariableSpeedCoils.cc
+++ src/EnergyPlus/VariableSpeedCoils.cc
@@ -4132,19 +4132,17 @@ namespace VariableSpeedCoils {
 
                         CoolCapAtPeak = (rhoair * VolFlowRate * (MixEnth - SupEnth)) + FanCoolLoad;
                         if (CoolCapAtPeak < 0) { // This conditional will also catch the initialization value, -999.0
-                            if (state.dataSize->FinalSysSizing(state.dataSize->CurSysNum).CoolDDNum > 0) {
-                                ShowWarningError(
-                                    state,
-                                    std::format(
-                                        "In calculating capacity for coil {} on design day {} when system cooling load is available, the air state would yield negative coil capacity sizing.",
-                                        varSpeedCoil.Name,
-                                        state.dataSize->FinalSysSizing(state.dataSize->CurSysNum).CoolDesDay));
+                            if (finalSysSizing.CoolDDNum > 0) {
+                                ShowWarningError(state,
+                                                 std::format("In calculating capacity for coil {} on design day {} when system cooling load is "
+                                                             "available, the air state would yield negative coil capacity sizing.",
+                                                             varSpeedCoil.Name,
+                                                             finalSysSizing.CoolDesDay));
                             } else {
-                                ShowWarningError(
-                                    state,
-                                    std::format(
-                                        "In calculating capacity for coil {} when system cooling load is not available, the air state would yield negative coil capacity sizing.",
-                                        varSpeedCoil.Name));
+                                ShowWarningError(state,
+                                                 std::format("In calculating capacity for coil {} when system cooling load is not available, the air "
+                                                             "state would yield negative coil capacity sizing.",
+                                                             varSpeedCoil.Name));
                             }
                             ShowContinueError(state, EnergyPlus::format("The air properties are: T_mix = {:.4R}", MixTemp));
                             ShowContinueError(state, EnergyPlus::format("                        T_supply = {:.4R}", SupTemp));
